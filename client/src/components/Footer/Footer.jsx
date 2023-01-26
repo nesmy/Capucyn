02c5b8996e772e7c4499c89d661d5163836b9859
nesmy <pnesmy@gmail.com>
2023-01-25 20:22:53 -0500

    save 1

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import "./Footer.scss";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
     return (
-        <div className="footer">Footer</div>
+        <div className="footer">
+            <div className="wrapper">
+                <div className="item">
+                    <Link className="link">Refund Policy</Link>
+                </div>
+                <div className="item">
+                    <Link className="link">Shipping Policy</Link>
+                </div>
+                <div className="item">
+                    <Link className="link">Privacy Policy</Link>
+                </div>
+            </div>
+        </div>
     );
 };
 
--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from "react";
 import "./Navbar.scss";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
     return (
-        <div className="navbar">Navbar</div>
+        <div className="navbar">
+            <div className="wrapper">
+                <div className="item">
+                    <Link className="link" to="/">About</Link>
+                </div>
+                <div className="item">
+                    <Link className="link" to="/projects">Projects</Link>
+                </div>
+                <div className="item">
+                    <Link className="link" to="/contact">Contact</Link>
+                </div>
+            </div>
+        </div>
     );
 };
 
